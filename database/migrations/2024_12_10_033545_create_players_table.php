aee65b3f57c96a6b6dc7e92f012700665eb18e72
Leonardo Lima <leonardo403@gmail.com>
2024-12-11 17:23:29 -0300

    algumas mudanças

---
+++ database/migrations/2024_12_10_033545_create_players_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('class_id');
+            $table->integer('xp')->default(1);
+            $table->boolean('confirmed')->default(false);
+            $table->timestamps();
+
+            $table->foreign('class_id')->references('id')->on('classes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
