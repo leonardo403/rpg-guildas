d9169abfd54eefa27be111d3588d6ad62b61d533
Leonardo Lima <leonardo403@gmail.com>
2024-12-14 16:56:34 -0300

    Implementada a lógica de acesso ao banco no repositório Player.

--- app/Repositories/PlayerRepository.php
+++ app/Repositories/PlayerRepository.php
@@ -2,9 +2,19 @@
 
 namespace App\Repositories;
 
-use PlayerRepositoryInterface;
+use App\Models\Player;
+use App\Repositories\Contracts\PlayerRepositoryInterface;
+use Illuminate\Support\Collection;
 
 class PlayerRepository implements PlayerRepositoryInterface
 {
-    
+    public function getConfirmedPlayers(): Collection
+    {
+        return Player::where('confirmed', true)->get();
+    }
+
+    public function findById(int $id): ?object
+    {
+        return Player::find($id);
+    }
 }
